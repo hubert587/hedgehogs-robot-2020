48d51401eed9b1a6e53c725890f187f990e63882
akeadey <akeadey2@gmail.com>
2020-01-15 18:56:26 -0500

    Created a new project for Swerve, copied the old commands and subsystems over

---
+++ robot-2020.1/src/main/cpp/commands/SpinToColor.cpp
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/SpinToColor.h"
+
+SpinToColor::SpinToColor() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void SpinToColor::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void SpinToColor::Execute() {}
+
+// Called once the command ends or is interrupted.
+void SpinToColor::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool SpinToColor::IsFinished() { return false; }
---
+++ robot-2020.1/src/main/cpp/subsystems/Blaster.cpp
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "subsystems/Blaster.h"
+
+Blaster::Blaster() {
+
+}
+
+void Blaster::Periodic() {
+  // Implementation of subsystem periodic method goes here.
+}
+
