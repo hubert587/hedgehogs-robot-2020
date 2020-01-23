4f08c9d2aa0e9df9d824a084457a59e7728c4078
hubert587 <53660098+hubert587@users.noreply.github.com>
2020-01-23 18:51:52 -0500

    Color Wheel Code Added

--- robot-2020.1/src/main/include/subsystems/WheelOfFortune.h
+++ robot-2020.1/src/main/include/subsystems/WheelOfFortune.h
@@ -8,6 +8,12 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include <Constants.h>
+#include <ctre/Phoenix.h>
+#include "rev/ColorSensorV3.h"
+#include <frc/smartdashboard/smartdashboard.h>
+#include <iostream>
+#include <string>
 
 class WheelOfFortune : public frc2::SubsystemBase {
 
@@ -18,7 +24,11 @@ class WheelOfFortune : public frc2::SubsystemBase {
    * Will be called periodically whenever the CommandScheduler runs.
    */
   void Periodic() override;
-
+  void SpinToColorInit();
+  void SpinToColor();
+  void SpinWheel3Init();
+  void SpinWheel3();
+  bool IsDone();
  private:
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
@@ -26,5 +36,12 @@ class WheelOfFortune : public frc2::SubsystemBase {
   //spinner
 
   //color detector
-
+  bool done = false;
+  int RedCount = 0;
+  bool onRed = false;
+  std::string Goal = "R";
+  std::string Current = "R";
+  WPI_TalonSRX m_motor{kColorWheelCanID};
+  static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
+  rev::ColorSensorV3 m_colorSensor{i2cPort};
 };
